22f07a41081a5bcc3b3fd612c698ccdd335ccf0d
Andranik <tednaaa@gmail.com>
2025-02-08 19:31:29 +0400

    basic setup for router & icons, but not working huh

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -1,18 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import RootView from '../views/RootView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'root',
+      component: RootView,
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: '/:profileId',
+      name: 'profile',
+      component: () => import('../views/ProfileView.vue'),
     },
   ],
 })
