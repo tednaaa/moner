fb288cfd08241f3ac459471eaae8dd910e0a46fe
Andranik <tednaaa@gmail.com>
2025-02-08 18:04:24 +0400

    configure primevue + tailwind + postcss

--- apps/frontend/src/views/HomeView.vue
+++ apps/frontend/src/views/HomeView.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
 <template>
   <main>
     <h1>This is Home page</h1>
+
+    <Button>Test</Button>
   </main>
 </template>
